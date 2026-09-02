c6bcdee182af8733c58e653c89d287d23b561b57
shauryT <shaurytandon2007@gmail.com>
2026-09-02 19:01:33 +0530

    Disable auto-reload for uvicorn server
    Changed the reload option to False for uvicorn.

--- backend/run.py
+++ backend/run.py
@@ -4,4 +4,9 @@ if __name__ == "__main__":
     print("Starting Qualcomm Edge AI Retail Intelligence Backend Server...")
     print("Interactive Swagger OpenAPI documentation: http://127.0.0.1:8000/docs")
     print("Live WebSocket Endpoint: ws://127.0.0.1:8000/ws/api/v1/dashboard/live")
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(
+    "app.main:app",
+    host="0.0.0.0",
+    port=8000,
+    reload=False
+)
